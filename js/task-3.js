0252b06efcaf374abaf61198126625f42cc14494
Prixons <163110375+Prixons@users.noreply.github.com>
2024-07-03 16:44:27 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var nameOutput = document.getElementById('name-output');
 
   nameInput.addEventListener('input', function() {
-    var name = nameInput.value.trim(); // Trim whitespace from input
+    var name = nameInput.value.trim(); 
     if (name === '') {
       nameOutput.textContent = 'Anonymous';
     } else {
